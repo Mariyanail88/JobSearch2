af4d54cda56cfdb95e4fab2dd9269ce586651fe1
Mariya <mariasherbakiva@mail.ru>
2024-07-22 16:16:14 +0600

    fixed error package

--- src/main/java/kg/attractor/jobsearch/dto/WorkExperienceInfoDto.java
+++ src/main/java/kg/attractor/jobsearch/dto/WorkExperienceInfoDto.java
@@ -1,11 +1,9 @@
-package main.java.kg.attractor.jobsearch.dto;
+package kg.attractor.jobsearch.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDateTime;
 
 @Data
 @Builder
@@ -13,7 +11,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class WorkExperienceInfoDto {
     private Integer id;
-    private Integer  resume_id;
+    private Integer resume_id;
     private Integer years;
     private String company_name;
     private String position;
--- src/main/java/kg/attractor/jobsearch/model/WorkExperienceInfo.java
+++ src/main/java/kg/attractor/jobsearch/model/WorkExperienceInfo.java
@@ -1,13 +1,12 @@
-package main.java.kg.attractor.jobsearch.model;
+package kg.attractor.jobsearch.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
-import lombok.AllArgsConstructor;
-import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -18,7 +17,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class WorkExperienceInfo {
     private Integer id;
-    private Integer  resume_id;
+    private Integer resume_id;
     private Integer years;
     private String company_name;
     private String position;
