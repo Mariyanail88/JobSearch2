5a7944c016bc180a69bfbd708814121566b5a89c
Mariya <mariasherbakiva@mail.ru>
2024-07-30 18:00:06 +0600

    fixed the class for working with "Resume". add new methods

--- src/main/java/kg/attractor/jobsearch/dao/ResumeDao.java
+++ src/main/java/kg/attractor/jobsearch/dao/ResumeDao.java
@@ -1,27 +1,79 @@
 package kg.attractor.jobsearch.dao;
 
-import kg.attractor.jobsearch.dao.mappers.ResumeMapper;
 import kg.attractor.jobsearch.model.Resume;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.support.DataAccessUtils;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
 public class ResumeDao {
-    private final JdbcTemplate jdbcTemplate;
+    private final JdbcTemplate template;
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
-    // Получение резюме по категории
-    public List<Resume> getResumesByCategory(int categoryId) {
-        String sql = "SELECT * FROM resumes WHERE category_id = ?";
-        return jdbcTemplate.query(sql, new ResumeMapper(), categoryId);
+    public List<Resume> getResume() {
+        String sql = """
+                select * from resumes
+                """;
+
+        return template.query(sql, new BeanPropertyRowMapper<>(Resume.class));
     }
 
-    // Получение резюме, созданных пользователем
-    public List<Resume> getResumesByUserId(int userId) {
-        String sql = "SELECT * FROM resumes WHERE applicant_id = ?";
-        return jdbcTemplate.query(sql, new ResumeMapper(), userId);
+    public Optional<Resume> getResumeById(long id) {
+        String sql = """
+                select * from resumes
+                where ID = ?
+                """;
+        return Optional.ofNullable(DataAccessUtils.singleResult(
+                template.query(sql, new BeanPropertyRowMapper<>(Resume.class), id)
+        ));
+    }
+
+    public Optional<Resume> getResumeByCategoryId(Integer categoryId) {
+        String sql = """
+                select * from RESUMES
+                where CATEGORY_ID = ?
+                """;
+        return Optional.ofNullable(DataAccessUtils.singleResult(
+                template.query(sql, new BeanPropertyRowMapper<>(Resume.class), categoryId)
+        ));
+    }
+
+    public List<Resume> getResumesByUserId(Integer userId) {
+        String sql = """
+                select * from RESUMES
+                where APPLICANT_ID = ?
+                """;
+        return template.query(sql, new BeanPropertyRowMapper<>(Resume.class), userId);
+    }
+
+    public void addResume(Resume resume) {
+        String sql = """
+                insert into RESUMES(APPLICANT_ID, NAME, CATEGORY_ID, SALARY, IS_ACTIVE, CREATED_DATE, UPDATE_TIME)
+                values (:applicantId, :name, :categoryId, :salary, :isActive,  :createdDate, :updateTime);
+                """;
+        namedParameterJdbcTemplate.update(sql, new MapSqlParameterSource()
+                .addValue("applicantId", resume.getApplicantId())
+                .addValue("name", resume.getName())
+                .addValue("categoryId", resume.getCategoryId())
+                .addValue("salary", resume.getSalary())
+                .addValue("isActive", resume.getIsActive())
+                .addValue("createdDate", resume.getCreatedDate())
+                .addValue("updateTime", resume.getUpdateTime()));
+    }
+
+    public void delete(Integer id) {
+        String sql = """
+                delete from RESUMES
+                where ID=?
+                """;
+        template.update(sql, id);
     }
 }
