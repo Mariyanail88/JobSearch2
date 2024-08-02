fcf89016642ca792190b5aea2622750dda3e1ade
Mariya <mariasherbakiva@mail.ru>
2024-08-02 15:49:19 +0600

    changed classes dto

--- src/main/java/kg/attractor/jobsearch/dto/ResumeDto.java
+++ src/main/java/kg/attractor/jobsearch/dto/ResumeDto.java
@@ -7,6 +7,10 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import java.time.LocalDateTime;
 
 @Data
@@ -15,11 +19,26 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class ResumeDto {
+
+    @NotNull(message = "Applicant ID cannot be null")
     Integer applicantId;
+
+    @NotBlank(message = "Name cannot be blank")
+    @Size(max = 100, message = "Name cannot be longer than 100 characters")
     String name;
+
+    @NotNull(message = "Category ID cannot be null")
     Integer categoryId;
+
+    @Min(value = 0, message = "Salary must be greater than or equal to 0")
     double salary;
+
+    @NotNull(message = "Active status cannot be null")
     Boolean isActive;
+
+    @NotNull(message = "Created date cannot be null")
     LocalDateTime createdDate;
+
+    @NotNull(message = "Update time cannot be null")
     LocalDateTime updateTime;
 }
--- src/main/java/kg/attractor/jobsearch/dto/UserDto.java
+++ src/main/java/kg/attractor/jobsearch/dto/UserDto.java
@@ -1,21 +1,52 @@
 package kg.attractor.jobsearch.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Max;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class UserDto {
+    @NotNull(message = "ID is mandatory")
     private Integer id;
+
+    @NotBlank(message = "Name is mandatory")
+    @Size(min = 2, max = 50, message = "Name must be between 2 and 50 characters")
     private String name;
+
+    @NotNull(message = "Age is mandatory")
+    @Min(value = 18, message = "Age must be at least 18")
+    @Max(value = 70, message = "Age must be less than or equal to 70")
     private Integer age;
+
+    @NotBlank(message = "Email is mandatory")
+    @Email(message = "Email should be valid")
     private String email;
+
+    @NotBlank(message = "Password is mandatory")
+    @Size(min = 4, max = 24, message = "Length must be >= 4 and <= 24")
+    @Pattern(regexp = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).+$", message = "Password should contain at least one uppercase letter, one number")
     private String password;
+
+    @NotBlank(message = "Phone number is mandatory")
+    @Pattern(regexp = "^\\+\\d{1} \\d{3} \\d{3}-\\d{2}-\\d{2}$", message = "Phone number must be in the format +X XXX XXX-XX-XX")
     private String phoneNumber;
+
     private String avatar;
+
+    @NotBlank(message = "Account type is mandatory")
     private String accountType;
 }
