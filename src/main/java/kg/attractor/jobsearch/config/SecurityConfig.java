47483d524c203bc9e09925df6beca6d6494fef3c
Mariya <mariasherbakiva@mail.ru>
2024-08-06 14:46:39 +0600

    added new class SecurityConfig

---
+++ src/main/java/kg/attractor/jobsearch/config/SecurityConfig.java
@@ -0,0 +1,57 @@
+package kg.attractor.jobsearch.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final DataSource dataSource;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        PasswordEncoder encoder = passwordEncoder();
+
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(encoder.encode("admin"))
+                .roles("ADMIN")
+                .authorities("FULL")
+                .build();
+
+        UserDetails guest = User.builder()
+                .username("guest")
+                .password(encoder.encode("guest"))
+                .roles("GUEST")
+                .authorities("READ_ONLY")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, guest);
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .passwordEncoder(passwordEncoder());
+    }
+}
