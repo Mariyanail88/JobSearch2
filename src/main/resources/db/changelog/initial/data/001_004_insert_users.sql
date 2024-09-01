7ac51dc022b4cd0ce75c5aa93f3edb4850485f69
Mariya <mariasherbakiva@mail.ru>
2024-09-01 18:43:40 +0600

    fix

---
+++ src/main/resources/db/changelog/initial/data/001_004_insert_users.sql
@@ -0,0 +1,22 @@
+INSERT INTO users (NAME, AGE, EMAIL, PASSWORD, PHONE_NUMBER, AVATAR, ACCOUNT_TYPE, ENABLED)
+VALUES
+    ('Алиса Джонсон', 29, 'alice@example.com', '$2a$10$T9MogeOPyAHHydm1qitCEummosIoVPZSjJc2SXufJJhjuCi8ieCrS', '1234567890', 'default_avatar.jpg', 'admin', true),
+    ('Чарли Браун', 40, 'charlie@example.com', '$2a$10$T9MogeOPyAHHydm1qitCEummosIoVPZSjJc2SXufJJhjuCi8ieCrS', '5555555555', 'No_Image_Available.jpg', 'authorized_user', true),
+    ('Джон Доу', 35, 'john.doe@example.com', '$2a$10$T9MogeOPyAHHydm1qitCEummosIoVPZSjJc2SXufJJhjuCi8ieCrS', '9876543210', 'default_avatar.jpg', 'guest', true),
+    ('Эмма Смит', 28, 'emma@example.com', '$2a$10$T9MogeOPyAHHydm1qitCEummosIoVPZSjJc2SXufJJhjuCi8ieCrS', '4444444444', 'No_Image_Available.jpg', 'guest', true),
+    ('Майкл Джонсон', 45, 'michael@example.com', '$2a$10$T9MogeOPyAHHydm1qitCEummosIoVPZSjJc2SXufJJhjuCi8ieCrS', '3333333333', 'default_avatar.jpg', 'employer', true),
+    ('Сара Ли', 33, 'sarah@example.com', '$2a$10$T9MogeOPyAHHydm1qitCEummosIoVPZSjJc2SXufJJhjuCi8ieCrS', '6666666666', 'No_Image_Available.jpg', 'employer', true),
+    ('Дэвид Браун', 38, 'david@example.com', '$2a$10$T9MogeOPyAHHydm1qitCEummosIoVPZSjJc2SXufJJhjuCi8ieCrS', '7777777777', 'default_avatar.jpg', 'applicant', true),
+    ('Эмили Уилсон', 31, 'emily@example.com', '$2a$10$T9MogeOPyAHHydm1qitCEummosIoVPZSjJc2SXufJJhjuCi8ieCrS', '8888888888', 'No_Image_Available.jpg', 'applicant', true),
+    ('Райан Миллер', 42, 'ryan@example.com', '$2a$10$T9MogeOPyAHHydm1qitCEummosIoVPZSjJc2SXufJJhjuCi8ieCrS', '9999999999', 'default_avatar.jpg', 'applicant', true),
+    ('Оливия Тейлор', 27, 'olivia@example.com', '$2a$10$T9MogeOPyAHHydm1qitCEummosIoVPZSjJc2SXufJJhjuCi8ieCrS', '2222222222', 'No_Image_Available.jpg', 'applicant', true),
+    ('Даниэль Мартинес', 36, 'daniel@example.com', '$2a$10$T9MogeOPyAHHydm1qitCEummosIoVPZSjJc2SXufJJhjuCi8ieCrS', '1111111111', 'default_avatar.jpg', 'applicant', true),
+    ('София Андерсон', 30, 'sophia@example.com', '$2a$10$T9MogeOPyAHHydm1qitCEummosIoVPZSjJc2SXufJJhjuCi8ieCrS', '1010101010', 'No_Image_Available.jpg', 'applicant', true),
+    ('Джеймс Кларк', 39, 'james@example.com', '$2a$10$T9MogeOPyAHHydm1qitCEummosIoVPZSjJc2SXufJJhjuCi8ieCrS', '1212121212', 'default_avatar.jpg', 'applicant', true),
+    ('Ава Харрис', 26, 'ava@example.com', '$2a$10$T9MogeOPyAHHydm1qitCEummosIoVPZSjJc2SXufJJhjuCi8ieCrS', '1313131313', 'No_Image_Available.jpg', 'applicant', true),
+    ('Мэтью Янг', 43, 'matthew@example.com', '$2a$10$T9MogeOPyAHHydm1qitCEummosIoVPZSjJc2SXufJJhjuCi8ieCrS', '1414141414', 'default_avatar.jpg', 'applicant', true),
+    ('Изабелла Лопес', 32, 'isabella@example.com', '$2a$10$T9MogeOPyAHHydm1qitCEummosIoVPZSjJc2SXufJJhjuCi8ieCrS', '1515151515', 'No_Image_Available.jpg', 'applicant', true),
+    ('Кристофер Кинг', 37, 'christopher@example.com', '$2a$10$T9MogeOPyAHHydm1qitCEummosIoVPZSjJc2SXufJJhjuCi8ieCrS', '1616161616', 'default_avatar.jpg', 'applicant', true),
+    ('Эмма Джонсон', 29, 'emma.johnson@example.com', '$2a$10$T9MogeOPyAHHydm1qitCEummosIoVPZSjJc2SXufJJhjuCi8ieCrS', '1717171717', 'No_Image_Available.jpg', 'applicant', true),
+    ('Александр Дэвис', 44, 'alexander@example.com', '$2a$10$T9MogeOPyAHHydm1qitCEummosIoVPZSjJc2SXufJJhjuCi8ieCrS', '1818181818', 'default_avatar.jpg', 'applicant', true),
+    ('Мэдисон Мур', 28, 'madison@example.com', '$2a$10$T9MogeOPyAHHydm1qitCEummosIoVPZSjJc2SXufJJhjuCi8ieCrS', '1919191919', 'No_Image_Available.jpg', 'applicant', true);
