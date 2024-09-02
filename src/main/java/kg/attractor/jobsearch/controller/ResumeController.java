d4ae8391dff079b737adb75e559c514d689edf89
Mariya <mariasherbakiva@mail.ru>
2024-09-03 02:50:24 +0600

    update methods and fails

--- src/main/java/kg/attractor/jobsearch/controller/ResumeController.java
+++ src/main/java/kg/attractor/jobsearch/controller/ResumeController.java
@@ -7,10 +7,12 @@ import kg.attractor.jobsearch.errors.UserNotFoundException;
 import kg.attractor.jobsearch.service.CategoriesService;
 import kg.attractor.jobsearch.service.ResumeService;
 import kg.attractor.jobsearch.service.UserService;
+import kg.attractor.jobsearch.service.impl.CustomUserDetails;
 import kg.attractor.jobsearch.util.MvcConrollersUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +20,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Slf4j
 @Controller
@@ -28,6 +32,7 @@ public class ResumeController {
     private final ResumeService resumeService;
     private final UserService userService;
     private final CategoriesService categoriesService;
+
 
 
     @GetMapping()
@@ -60,14 +65,36 @@ public class ResumeController {
     public String showCreateResumeForm(Model model, Authentication authentication) {
         model.addAttribute("resume", new ResumeDto());
         MvcConrollersUtil.authCheck(model, authentication);
+
+        // Получение списка категорий и добавление его в модель
+        List<CategoryDto> categories = categoriesService.getCategories();
+        model.addAttribute("categories", categories);
+
         return "resumes/create_resume";
     }
 
 
     @PostMapping("create")
-    public String createResume(@ModelAttribute("resume") ResumeDto resumeDto, Authentication authentication) {
+    public String createResume(@ModelAttribute ResumeDto resumeDto, Authentication authentication) {
+        // Получение ID Соискателя из аутентификационных данных
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        int applicantId = getApplicantIdFromUserDetails(userDetails);
+
+        // Установка ID Соискателя в DTO
+        resumeDto.setApplicantId(applicantId);
+
+        // Логика сохранения резюме
         resumeService.createResume(resumeDto);
-        return "redirect:/auth/profile";
+
+        // Перенаправление на профиль создателя
+        return "redirect:/auth/profile/" + applicantId;
+    }
+
+    private int getApplicantIdFromUserDetails(UserDetails userDetails) {
+        if (userDetails instanceof CustomUserDetails) {
+            return ((CustomUserDetails) userDetails).getApplicantId();
+        }
+        throw new IllegalArgumentException("UserDetails не содержит информацию о соискателе");
     }
 
     @GetMapping("edit/{resumeId}")
