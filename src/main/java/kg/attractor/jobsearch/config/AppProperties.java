2eb825a354ba1b4faf29199e1ec9719c02283cb0
Mariya <mariasherbakiva@mail.ru>
2024-09-13 18:59:21 +0600

    add new class

---
+++ src/main/java/kg/attractor/jobsearch/config/AppProperties.java
@@ -0,0 +1,27 @@
+package kg.attractor.jobsearch.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+@Getter
+@Setter
+@Component
+@ConfigurationProperties(prefix = "app")
+public class AppProperties {
+    private String avatarDir;
+    private Locale locale;
+
+
+
+
+@Setter
+@Getter
+    public static class Locale {
+        private String defaultLang;
+        private String paramName;
+
+
+
+    }
+}
